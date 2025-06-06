0f76d72d377bbf6c7a88d98797d62a8081499699
Sophia Snipes <snipeso@users.noreply.github.com>
2025-06-06 11:32:13 +0200

    make robust max window

--- +sprep/+monoch/adjust_edges_to_baseline.m
+++ +sprep/+monoch/adjust_edges_to_baseline.m
@@ -14,10 +14,13 @@ end
 % identify the first points from the original cut that returns to baseline
 Indices = 1:numel(Signal); 
 BaselineSignal =  Signal >=Edges(1) & Signal <=Edges(2); % all the points within the identified range
-MaxWindow = (Indices >= StartCut-MaxPntsSearchWindow/2) & (Indices <= EndCut + MaxPntsSearchWindow/2); % all the points within an acceptable distance from the original cut
+% MaxWindow = (Indices >= StartCut-MaxPntsSearchWindow/2) & (Indices <= EndCut + MaxPntsSearchWindow/2); % all the points within an acceptable distance from the original cut
 
-WindowStart = find(Indices<=StartCut & BaselineSignal & MaxWindow, 1, 'last');
-WindowEnd = find(Indices>=EndCut & BaselineSignal & MaxWindow, 1, 'first');
+% WindowStart = find(Indices<=StartCut & BaselineSignal & MaxWindow, 1, 'last');
+% WindowEnd = find(Indices>=EndCut & BaselineSignal & MaxWindow, 1, 'first');
+
+WindowStart = find(Indices<=StartCut & BaselineSignal, 1, 'last');
+WindowEnd = find(Indices>=EndCut & BaselineSignal, 1, 'first');
 
 % if unsuccessful, just just use the whole max window
 if isempty(WindowStart)
@@ -29,3 +32,10 @@ if isempty(WindowEnd)
 end
 
 
+% Make sure it's not outside maximum allowable extension range
+
+MinStart = StartCut-MaxPntsSearchWindow/2;
+MaxEnd = EndCut + MaxPntsSearchWindow/2';
+
+WindowStart = max(MinStart, WindowStart);
+WindowEnd = min(MaxEnd, WindowEnd);
