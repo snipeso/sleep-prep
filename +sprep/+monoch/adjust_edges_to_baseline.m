c3210c50648bdbef271c35eb986ef385af9cfae0
Sophia Snipes <snipeso@users.noreply.github.com>
2025-06-06 11:46:18 +0200

    now it sets the edge to a point within a range, but

--- +sprep/+monoch/adjust_edges_to_baseline.m
+++ +sprep/+monoch/adjust_edges_to_baseline.m
@@ -9,33 +9,52 @@ end
 % [WindowStart, WindowEnd] = return_to_baseline(Signal, StartCut, EndCut, Quantile, MaxPntsSearchWindow)
 %
 % resets the edges of window so that they are around the median of the
-% whole signal. 
+% whole signal.
 
 % identify the first points from the original cut that returns to baseline
-Indices = 1:numel(Signal); 
+Indices = 1:numel(Signal);
 BaselineSignal =  Signal >=Edges(1) & Signal <=Edges(2); % all the points within the identified range
-% MaxWindow = (Indices >= StartCut-MaxPntsSearchWindow/2) & (Indices <= EndCut + MaxPntsSearchWindow/2); % all the points within an acceptable distance from the original cut
 
-% WindowStart = find(Indices<=StartCut & BaselineSignal & MaxWindow, 1, 'last');
-% WindowEnd = find(Indices>=EndCut & BaselineSignal & MaxWindow, 1, 'first');
+MinStart = StartCut-MaxPntsSearchWindow/2;
+if MinStart <1
+    MinStart = 1;
+end
+
+MaxEnd = EndCut + MaxPntsSearchWindow/2';
+if MaxEnd > numel(Signal)
+    MaxEnd = numel(Signal);
+end
 
 WindowStart = find(Indices<=StartCut & BaselineSignal, 1, 'last');
 WindowEnd = find(Indices>=EndCut & BaselineSignal, 1, 'first');
 
 % if unsuccessful, just just use the whole max window
 if isempty(WindowStart)
-    WindowStart = find(MaxWindow, 1, 'first');
+    WindowStart = MinStart;
 end
 
 if isempty(WindowEnd)
-    WindowEnd = find(Indices, 1, 'last');
+    WindowEnd = MaxEnd;
 end
 
 
-% Make sure it's not outside maximum allowable extension range
+% Make sure it's not outside maximum allowable extension range. If it is,
+% then just take the edges that minimize the distance from the middle of
+% the signal
 
-MinStart = StartCut-MaxPntsSearchWindow/2;
-MaxEnd = EndCut + MaxPntsSearchWindow/2';
+Midpoint = mean(Edges); % the theoretical 0 of the channel. Using this instead of actual 0, because there might be large offsets
 
-WindowStart = max(MinStart, WindowStart);
-WindowEnd = min(MaxEnd, WindowEnd);
+if WindowStart < MinStart
+    [~, Idx] = min(Signal(MinStart:StartCut)-Midpoint);
+    WindowStart = Idx+MinStart;
+    warning('untested code in adjust_edges_to_baseline')
+end
+
+if WindowEnd > MaxEnd
+    [~, Idx] = min(Signal(EndCut:MaxEnd)-Midpoint);
+    WindowEnd = Idx+EndCut;
+        warning('untested code in adjust_edges_to_baseline')
+end
+
+
+
