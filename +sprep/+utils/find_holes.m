3d62b00358dc43779dccf32fe7a0041cf555dc4a
Sophia Snipes <snipeso@users.noreply.github.com>
2025-07-18 16:49:45 +0200

    remove epochs with holes

---
+++ +sprep/+utils/find_holes.m
@@ -0,0 +1,24 @@
+function Holes = find_holes(Artefacts, Chanlocs, MinNeighbors)
+% identifies any time there's artefacts in adjacent channels too far apart
+% for good interpolation
+arguments
+    Artefacts % is a channel x time matrix, with 1 indicating an artefact
+    Chanlocs
+    MinNeighbors = 2;
+end
+
+Neighbors = sprep.utils.find_neighbors(Chanlocs);
+EdgeChannels = find(sum(Neighbors)<=5);
+
+Holes = zeros(size(Artefacts));
+
+for ChannelIdx = 1:numel(Chanlocs)
+    if ismember(ChannelIdx, EdgeChannels) % its ok if outer edge channels are poorly interpolated
+        continue
+    end
+
+    ChNeighbors = Neighbors(ChannelIdx, :);
+
+    nGoodNeighbors = sum(Artefacts(ChNeighbors, :)==0);
+    Holes(ChannelIdx, :) = nGoodNeighbors < MinNeighbors;
+end
