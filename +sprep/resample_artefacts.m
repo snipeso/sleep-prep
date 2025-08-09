3344183bc799aaebcf4c19af07583b488c86d11a
Sophia Snipes <snipeso@users.noreply.github.com>
2025-08-09 11:37:14 +0200

    make more generic

--- +sprep/resample_artefacts.m
+++ +sprep/resample_artefacts.m
@@ -1,7 +1,15 @@
-function ArtefactsCell = resample_artefacts(ArtefactsCell, SampleRate, EpochLength, MaxOldPoints, MaxNewPoints)
+function ArtefactsCell = resample_artefacts(ArtefactsCell, SampleRate, EpochLength, MaxOldPoints, MaxNewPoints, OldPeriod)
+arguments
+    ArtefactsCell
+    SampleRate
+    EpochLength
+    MaxOldPoints
+    MaxNewPoints
+    OldPeriod = 1/SampleRate; % most detectors are for every sample point, but if not, specify what the period was
+end
 
 
 for ArtefactIdx = 1:numel(ArtefactsCell)
     ArtefactsCell{ArtefactIdx} = sprep.resample_matrix(ArtefactsCell{ArtefactIdx}, ...
-        1/SampleRate, EpochLength, SampleRate, MaxOldPoints, MaxNewPoints); 
+        OldPeriod, EpochLength, SampleRate, MaxOldPoints, MaxNewPoints); 
 end
