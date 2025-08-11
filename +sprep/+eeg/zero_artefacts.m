feedf06fea67a19162a55d3cb115766fc751e2a6
Sophia Snipes <snipeso@users.noreply.github.com>
2025-08-11 12:06:33 +0200

    little fix

--- +sprep/+eeg/zero_artefacts.m
+++ +sprep/+eeg/zero_artefacts.m
@@ -6,7 +6,7 @@ arguments
     EpochLength = floor(size(EEG.data, 2)/size(Artefacts, 2));
 end
 
-if size(Artefacts) ~= size(EEG.data)
+if any(size(Artefacts) ~= size(EEG.data))
     Artefacts = sprep.resample_matrix(Artefacts, EpochLength, [], EEG.srate, size(EEG.data, 2));
 end
 
