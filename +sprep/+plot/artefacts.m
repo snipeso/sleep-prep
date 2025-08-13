ecbcd33c6258f2c7ef38fbd2fd545f603b5a8d5e
Sophia Snipes <snipeso@users.noreply.github.com>
2025-08-13 21:39:06 +0200

    plot artefacts optionally resizes matrix

--- +sprep/+plot/artefacts.m
+++ +sprep/+plot/artefacts.m
@@ -1,4 +1,8 @@
-function artefacts(EEG, Artefacts)
+function artefacts(EEG, Artefacts, EpochLength)
+
+if any(size(Artefacts) ~= size(EEG.data))
+    Artefacts = sprep.resample_matrix(Artefacts, EpochLength, [], EEG.srate, size(EEG.data, 2));
+end
 
 ArtefactsData = EEG.data;   
 ArtefactsData(Artefacts==0) = nan;
