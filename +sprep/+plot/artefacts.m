e328598ecf5c2a614a968765419b9340c416ddcc
Sophia Snipes <snipeso@users.noreply.github.com>
2025-08-18 14:06:52 +0200

    plot just eeg

--- +sprep/+plot/artefacts.m
+++ +sprep/+plot/artefacts.m
@@ -1,9 +1,17 @@
 function artefacts(EEG, Artefacts, EpochLength)
+arguments
+    EEG
+    Artefacts = [];
+    EpochLength = 20;
+end
 
-if any(size(Artefacts) ~= size(EEG.data))
+if isempty(Artefacts)
+    sprep.plot.eeglab_scroll(EEG)
+    return
+elseif any(size(Artefacts) ~= size(EEG.data))
     Artefacts = sprep.resample_matrix(Artefacts, EpochLength, [], EEG.srate, size(EEG.data, 2));
 end
 
 ArtefactsData = EEG.data;   
 ArtefactsData(Artefacts==0) = nan;
-sprep.plot.eeglab_scroll(EEG, ArtefactsData)
+sprep.plot.eeglab_scroll(EEG, ArtefactsData, EpochLength)
