5356f2f6100a2a3fc2160a31d6bd0e2affbd56b7
Sophia Snipes <snipeso@users.noreply.github.com>
2025-08-29 16:07:07 +0200

    Update cut_out_artefacts.m

--- +sprep/+eeg/cut_out_artefacts.m
+++ +sprep/+eeg/cut_out_artefacts.m
@@ -1,13 +1,21 @@
 function EEG = cut_out_artefacts(EEG, RemoveEpochs, EpochLength)
 
-[Starts, Ends] = sprep.utils.data2windows(RemoveEpochs);
+% [Starts, Ends] = sprep.utils.data2windows(RemoveEpochs);
+% 
+% Windows = [Starts'-1, Ends']*EpochLength;
+% 
+% % make sure to capture any of that lingering EEG at the end when its not
+% % exactly a multiple of the epoch lengths
+% if Ends(end)==numel(RemoveEpochs)
+%     Windows(end) = size(EEG.data, 2)/EEG.srate;
+% end
+% 
+% EEG = pop_select(EEG, 'notime', Windows);
 
-Windows = [Starts'-1, Ends']*EpochLength;
+UpsampledArtefacts = sprep.utils.resample_array(RemoveEpochs, EpochLength, [], EEG.srate, size(EEG.data, 2), 'max');
+EndArtefacts = numel(RemoveEpochs)*EpochLength*EEG.srate;
+UpsampledArtefacts(:, EndArtefacts:end) = 1; % remove last seconds of data
 
-% make sure to capture any of that lingering EEG at the end when its not
-% exactly a multiple of the epoch lengths
-if Ends(end)==numel(RemoveEpochs)
-    Windows(end) = size(EEG.data, 2)/EEG.srate;
-end
+EEG.data(:, UpsampledArtefacts) = [];
 
-EEG = pop_select(EEG, 'notime', Windows);
+EEG = eeg_checkset(EEG); % it won't be happy
