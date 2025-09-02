4af6302ccbddb7d01928aa37665fafe0c75cb758
Sophia Snipes <snipeso@users.noreply.github.com>
2025-09-02 17:36:18 +0200

    more options to merge

--- +sprep/merge_artefacts.m
+++ +sprep/merge_artefacts.m
@@ -1,6 +1,19 @@
-function AllArtefacts = merge_artefacts(ArtefactsCell)
+function [AllArtefacts, X, Y] = merge_artefacts(ArtefactsCell, MergeType)
+arguments
+    ArtefactsCell
+    MergeType = 'or';
+end
 
 AllArtefacts = false(size(ArtefactsCell{1}));
 for ArtefactIdx = 1:numel(ArtefactsCell)
-    AllArtefacts = AllArtefacts | ArtefactsCell{ArtefactIdx};
+    switch MergeType
+        case 'or'
+            AllArtefacts = AllArtefacts | ArtefactsCell{ArtefactIdx};
+        case 'sum'
+        A = ArtefactsCell{ArtefactIdx};
+        X = min(size(A, 1), size(AllArtefacts, 1));
+        Y = min(size(A, 2), size(AllArtefacts, 2));
+            AllArtefacts = AllArtefacts(1:X, 1:Y) + A(1:X, 1:Y);
+
+    end
 end
--- +sprep/remove_artefacts.m
+++ +sprep/remove_artefacts.m
@@ -1,7 +1,5 @@
 function Power = remove_artefacts(Power, Artefacts)
 for ChannelIdx = 1:size(Artefacts, 1)
-
     Power(ChannelIdx, Artefacts(ChannelIdx, :), :) = nan;
-
 end
 end
