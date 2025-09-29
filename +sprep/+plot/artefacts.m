70668954ffa11cfc8e7328430b49923cf6c57450
Sophia Snipes <code@sophia.science>
2025-09-29 15:54:03 +0200

    fix exceptions to plotting

--- +sprep/+plot/artefacts.m
+++ +sprep/+plot/artefacts.m
@@ -5,10 +5,17 @@ arguments
     EpochLength = 20;
 end
 
+
 if isempty(Artefacts)
     sprep.plot.eeglab_scroll(EEG)
     return
-elseif any(size(Artefacts) ~= size(EEG.data))
+end
+
+if size(Artefacts, 1) ~= size(EEG.data, 1)
+    error('mismatch in dimentions of artefacts and eeg')
+end
+
+if size(Artefacts, 2) ~= size(EEG.data, 2)
     Artefacts = sprep.resample_matrix(Artefacts, EpochLength, [], EEG.srate, size(EEG.data, 2));
 end
 
